a6a635161366b1ecc9bc1e5f942399ce019ad7d2
Andrew <andrew@chemies.com>
2020-12-10 22:02:38 -0700

    Stubbing out Day 11

---
+++ src/main/java/com/chemies/AoC2020/data/InputsDay11.java
@@ -0,0 +1,7 @@
+package com.chemies.AoC2020.data;
+
+public class InputsDay11 {
+    public static String INPUTS = "day11Inputs.txt";
+    public static String SAMPLE = "day11Sample.txt";
+    //public static String SAMPLEB = "day11SampleB.txt";
+}
---
+++ src/main/java/com/chemies/AoC2020/day/Day11.java
@@ -0,0 +1,30 @@
+package com.chemies.AoC2020.day;
+
+import com.chemies.AoC2020.data.InputsDay11;
+
+class Day11 extends AbstractDay {
+    @Override
+    public void executePartA() {
+        int result = partA(InputsDay11.INPUTS);
+        System.out.println("PartA Ans: " + formatAnswer(result));
+    }
+
+    int partA(String inputs) {
+        return 0;
+    }
+
+    @Override
+    public void executePartB() {
+        int result = partB(InputsDay11.INPUTS);
+        System.out.println("PartB Ans: " + formatAnswer(result));
+    }
+
+    int partB(String inputs) {
+        return 0;
+    }
+
+    @Override
+    protected String getName() {
+        return "Day 11";
+    }
+}
