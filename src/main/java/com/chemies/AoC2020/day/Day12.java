c3ac220fb8dc228581bba5ed69fc4426b3b6edf7
Andrew <andrew@chemies.com>
2020-12-12 00:49:20 -0700

    Day 12 part b

--- src/main/java/com/chemies/AoC2020/day/Day12.java
+++ src/main/java/com/chemies/AoC2020/day/Day12.java
@@ -9,11 +9,11 @@ import static java.lang.Math.abs;
 public class Day12 extends AbstractDay {
     @Override
     public void executePartA() {
-        int result = partA(InputsDay12.INPUTS);
+        long result = partA(InputsDay12.INPUTS);
         System.out.println("PartA Ans: " + formatAnswer(result));
     }
 
-    int partA(String filename) {
+    long partA(String filename) {
         ImmutableList<String> list = _fileHelper.fileToStringList(filename);
         var currentHeading = new Object() {
             int val = 90;
@@ -52,8 +52,11 @@ public class Day12 extends AbstractDay {
         return calcManhattanDist(currentHeading.pos);
     }
 
-    private int calcManhattanDist(Pair<Integer, Integer> pos) {
-        return abs(pos.getValue0()) + abs(pos.getValue1());
+    private long calcManhattanDist(Pair<Integer, Integer> pos) {
+        long a = abs(pos.getValue0());
+        long b = abs(pos.getValue1());
+
+        return a + b;
 
     }
 
@@ -119,13 +122,86 @@ public class Day12 extends AbstractDay {
 
     @Override
     public void executePartB() {
-        int result = partB(InputsDay12.INPUTS);
+        long result = partB(InputsDay12.INPUTS);
         System.out.println("PartB Ans: " + formatAnswer(result));
     }
 
-    int partB(String filename) {
-        return 0;
+    long partB(String filename) {
+        ImmutableList<String> list = _fileHelper.fileToStringList(filename);
+        var currentHeading = new Object() {
+            int val = 90;
+            Pair<Integer, Integer> pos = new Pair<>(0, 0);
+            Pair<Integer, Integer> way = Pair.with(10, 1);
+        };
+
+        list.forEach(line -> {
+            Pair<Character, Integer> instruct = readInstruct(line);
+            Integer delta = instruct.getValue1();
+            switch (instruct.getValue0()) {
+                case 'N':
+                    currentHeading.way = moveNorth(delta, currentHeading.way);
+                    break;
+                case 'S':
+                    currentHeading.way = moveSouth(delta, currentHeading.way);
+                    break;
+                case 'E':
+                    currentHeading.way = moveEast(delta, currentHeading.way);
+                    break;
+                case 'W':
+                    currentHeading.way = moveWest(delta, currentHeading.way);
+                    break;
+                case 'L':
+                    currentHeading.way = turnLeft(delta, currentHeading.way);
+                    break;
+                case 'R':
+                    currentHeading.way = turnRight(delta, currentHeading.way);
+                    break;
+                case 'F':
+                    currentHeading.pos = moveForwardTowardsWay(delta, currentHeading.pos, currentHeading.way);
+                    break;
+            }
+        });
+        System.out.println(currentHeading.pos);
+        return calcManhattanDist(currentHeading.pos);
     }
+
+
+    private Pair<Integer, Integer> moveForwardTowardsWay(Integer delta, Pair<Integer, Integer> pos, Pair<Integer, Integer> way) {
+
+        return Pair.with(pos.getValue0() + (delta * way.getValue0()), pos.getValue1() + (delta * way.getValue1()));
+    }
+
+
+    public Pair<Integer, Integer> turnLeft(int delta, Pair<Integer, Integer> pos) {
+
+        return rotate(delta, pos);
+    }
+
+    public Pair<Integer, Integer> turnRight(int delta, Pair<Integer, Integer> pos) {
+        return rotate(delta * -1, pos);
+    }
+
+    private Pair<Integer, Integer> rotate(int delta, Pair<Integer, Integer> pos) {
+        int x = 0;
+        int y = 0;
+        int corrected = correctHeading(delta);
+        if (corrected == 90) {
+            x = pos.getValue1() * -1;
+            y = pos.getValue0();
+        }
+
+        if (corrected == 180) {
+            x = pos.getValue0() * -1;
+            y = pos.getValue1() * -1;
+        }
+
+        if (corrected == 270) {
+            x = pos.getValue1();
+            y = pos.getValue0() * -1;
+        }
+        return Pair.with(x, y);
+    }
+
 
     @Override
     protected String getName() {
--- src/test/java/com/chemies/AoC2020/day/Day12Test.java
+++ src/test/java/com/chemies/AoC2020/day/Day12Test.java
@@ -1,6 +1,7 @@
 package com.chemies.AoC2020.day;
 
 import com.chemies.AoC2020.data.InputsDay12;
+import org.javatuples.Pair;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -16,13 +17,27 @@ class Day12Test {
 
     @Test
     void executePartA() {
-        assertEquals(25
+        assertEquals(25L
                 , _subject.partA(InputsDay12.SAMPLE));
     }
 
     @Test
     void executePartB() {
-        assertEquals(7 * 5
+        assertEquals(286L
                 , _subject.partB(InputsDay12.SAMPLE));
+    }
+
+    @Test
+    void turnRight90() {
+        Pair<Integer, Integer> actual = _subject.turnRight(90, Pair.with(10, 4));
+        assertEquals(-10, actual.getValue1());
+        assertEquals(4, actual.getValue0());
+    }
+
+    @Test
+    void turnRight180() {
+        Pair<Integer, Integer> actual = _subject.turnRight(180, Pair.with(10, 4));
+        assertEquals(-10, actual.getValue1());
+        assertEquals(4, actual.getValue0());
     }
 }
